ddf1cb66f205a56959d4178560396bbab31817a3
Devesh Jadon <jadon.devesh98@gmail.com>
2018-04-27 14:31:19 +0530

    Question schema (#57)
    Questing schema updated: updated the options to have independent information and other keys to manage the model properly

--- server/api/models/question.js
+++ server/api/models/question.js
@@ -2,9 +2,34 @@ const mongoose = require("mongoose");
 
 // Question Schema
 const QuestionSchema = mongoose.Schema({
-  question: { type: String },
-  answer: { type: String },
-  options: [{ type: String }]
+  title: {
+    type: String,
+    default: null
+  },
+  options: [
+    {
+      value: {
+        type: String,
+        default: null
+      },
+      is_correct: {
+        type: Boolean,
+        default: false
+      }
+    }
+  ],
+  is_deleted: {
+    type: Boolean,
+    default: false
+  },
+  created_at: {
+    type: Date,
+    default: Date.now
+  },
+  updated_at: {
+    type: Date,
+    default: Date.now
+  }
 });
 
 // Question Model
